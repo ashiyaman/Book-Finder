0edd6ad9cad4eaa130a6cb46e8d951172e7f5a9d
Ashiya Banu <ashiya1005@gmail.com>
2019-03-03 02:19:35 +1000

    Displayed results

--- main.js
+++ main.js
@@ -1,8 +1,59 @@
 /******************* search for books using fetch api ********************/
 
 let searchBooks = () =>{
-  const QUERY = document.getElementById('query').value;
+  const QUERY = document.querySelector('#query').value;
   fetch(`https://www.googleapis.com/books/v1/volumes?q=${QUERY}`)
     .then (res => res.json())
-    .then (data => console.log(data.items));
+    .then (data => booksContainer(data.items))
+}
+
+/******************** display books container  **********************/
+let booksContainer = (items) =>{
+  let displayBookElement = document.querySelector('.display-container');
+  displayBookElement.classList.remove('hidden');
+  items.forEach ((item, index) =>{
+    let bookTemplate = createBookTemplate(item.volumeInfo);
+    displayBookElement.appendChild(bookTemplate);
+  })
+}
+
+/*********************** individual book template *******************************/
+let createBookTemplate = (item) =>{
+  console.log(item);
+  let parentBookElement = document.createElement('div');
+  parentBookElement.setAttribute('class', 'parentBook');
+
+  let imageElement = document.createElement('img');
+  imageElement.setAttribute('src', item.imageLinks.thumbnail);
+
+  let bookInfoElement = document.createElement('div');
+  bookInfoElement.setAttribute('class', 'bookInfo');
+
+  let bookTitleElement = document.createElement('h3');
+  bookTitleElement.innerHTML = item.title;
+  bookTitleElement.setAttribute('class', 'title');
+
+  let bookAuthorElement = document.createElement('P');
+  bookAuthorElement.innerHTML = 'By: ' + item.authors;
+
+  let bookPublisherElement = document.createElement('p');
+  bookPublisherElement.innerHTML = 'Published By: ' + item.publisher;
+
+  let bookInfoButton = document.createElement('button');
+  bookInfoButton.innerHTML = 'See this book';
+  bookInfoButton.setAttribute('click', 'viewBook()');
+  bookInfoButton.setAttribute('class', 'float-right');
+
+  bookInfoElement.appendChild(bookTitleElement);
+  bookInfoElement.appendChild(bookAuthorElement);
+  bookInfoElement.appendChild(bookPublisherElement);
+  bookInfoElement.appendChild(bookInfoButton);
+  parentBookElement.appendChild(imageElement);
+  parentBookElement.appendChild(bookInfoElement);
+
+  return parentBookElement;
+}
+
+let viewBook = () =>{
+  console.log('here');
 }
